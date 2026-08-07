934abc39989c4ab70248a036d2d749d3f6752e80
codeholic08 <mmaazr99@gmail.com>
2026-08-06 21:28:29 -0400

    Load AWS config from env vars with safe defaults
    Placeholder literal region string was invalid and crashed boto3
    client creation at import time, breaking the app even when routes
    never used AWS.

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    AWS_REGION = '<AWS_REGION>'
-    S3_BUCKET = '<S3_BUCKET>'
-    ES_HOST = '<ES_HOST>'
-    ES_INDEX = '<ES_INDEX>'
-    LEX_BOT_ID = '<BOT_ID>'
-    LEX_BOT_ALIAS_ID = '<BOT_ALIAS_ID>'
+    AWS_REGION = os.environ.get('AWS_REGION', 'us-east-1')
+    S3_BUCKET = os.environ.get('S3_BUCKET', '')
+    ES_HOST = os.environ.get('ES_HOST', '')
+    ES_INDEX = os.environ.get('ES_INDEX', '')
+    LEX_BOT_ID = os.environ.get('LEX_BOT_ID', '')
+    LEX_BOT_ALIAS_ID = os.environ.get('LEX_BOT_ALIAS_ID', '')
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
